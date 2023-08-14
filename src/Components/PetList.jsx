11bdd5379d3d58acd6856f4473d1ae9a9a1bdd4c
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-08-14 18:57:23 -0400

    fixed pet list glitch

--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -3,6 +3,7 @@ export const PetList = ({ employeeId }) => {
   const petsApi = "https://one0-2-vet-api.onrender.com/api/pets";
 
   const [pets, setPets] = useState([]);
+  const [hasData, setHasData] = useState(false)
 
   useEffect(() => {
     fetch(petsApi)
@@ -12,19 +13,23 @@ export const PetList = ({ employeeId }) => {
           (pet) => pet.employeeId === employeeId
         );
         setPets(employeePets);
+        setHasData(true)
       })
       .catch((err) => {
         console.log(err);
+        setHasData(true)
       });
-  }, []);
+  }, [employeeId]);
 
   return (
     <aside className="pets-list">
-      {pets.length > 0 ? (
-        <p>{pets.map((pet) => pet.name).join(", ")}</p>
-      ) : (
-        <p>No pets listed for this employee.</p>
-      )}
+      {hasData ? (
+        pets.length > 0 ? (
+          <p>{pets.map((pet) => pet.name).join(", ")}</p>
+        ) : (
+          <p>No pets listed for this employee.</p>
+        )
+      ) : null}
     </aside>
   );
 };
